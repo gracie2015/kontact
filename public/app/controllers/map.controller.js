bda01198ca3e9f829e5d0e0374a57f30d3f8d993
Gracie_liu <gracieforjob@outlook.com>
2015-07-17 16:08:53 -0700

    enable google maps and add marker

--- public/app/controllers/map.controller.js
+++ public/app/controllers/map.controller.js
@@ -2,9 +2,36 @@
 
 var mapController = kontactApp.controller('MapCtrl', function($scope) {
   $scope.center = {
-    latitude: 45,
-    longitude: -73
+    latitude: 47.5,
+    longitude: -122.2
   };
-  $scope.markers = [];
-  $scope.zoom = 8;
+  $scope.markers = [
+    {
+      id: 0,
+      coords: {
+        latitude: 47.618819,
+        longitude: -122.168126
+      },
+      data: 'Bellevue'
+    },
+    {
+      id: 1,
+      coords:{
+        latitude: 47.673406,
+        longitude: -122.119031
+      },
+      data: 'Redmond'
+    }
+    
+    ];
+  $scope.zoom = 9;
+  
+  $scope.selectedMarker = null;
+  $scope.showInfoWindow = false;
+  
+  $scope.markerClick = function (marker, event, obj) {
+    $scope.selectedMarker = obj;
+    $scope.showInfoWindow = true;
+  };
+
 });
