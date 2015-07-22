6daad63effff9eb130a22f3c2ca88745c3559639
Gracie_liu <gracieforjob@outlook.com>
2015-07-22 13:22:05 -0700

    modify selected user, resolve and inject for passing parameters to modale.

--- public/app/controllers/list.controller.js
+++ public/app/controllers/list.controller.js
@@ -38,6 +38,46 @@ var addUserDlgController = kontactApp.controller('AddUserDlgCtrl', function($sco
   
 });
 
+var modifyUserDlgController = kontactApp.controller('ModifyUserDlgCtrl', function($scope, $http, $modalInstance, selectedId) {
+  $scope.id = selectedId;
+  $scope.firstName = '';
+  $scope.lastName = '';
+  $scope.gender = '';
+  $scope.cell = '';
+  $scope.email = '';
+  $scope.location = '';
+  
+  $scope.modifyUser = function() {
+    alert("inside modifyUser, id is  "+$scope.id);//undefined...seems parameter selectedId not passed.
+    
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': $scope.location }, function (results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        var coords = {
+          latitude: results[0].geometry.location.lat(),
+          longitude: results[0].geometry.location.lng()
+        };
+       
+        $http.post(('/users/' + $scope.id), {
+          firstname: $scope.firstName,
+          lastname: $scope.lastName,
+          gender: $scope.gender,
+          cell: $scope.cell,
+          email: $scope.email,
+          coords: coords,
+          location: $scope.location
+          
+        }).success(function(data) {
+          $modalInstance.close();
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  };
+  
+});
+
 var listController = kontactApp.controller('ListCtrl', function($scope, $http, $modal) {
   $scope.getUsers = function() {
     $http.get('/users/')
@@ -68,12 +108,33 @@ var listController = kontactApp.controller('ListCtrl', function($scope, $http, $
     });
   };
   
+  
+  
   $scope.onChange = function(idx) {
     $scope.selectedId = idx;
   };
   
+  $scope.openModifyUserDlg = function() {
+    alert("hello: " + $scope.selectedId);// hello 2...
+    var modalInstance = $modal.open({
+      animation: true,
+      templateUrl: 'modifyUserDlg.html',
+      controller: 'ModifyUserDlgCtrl',
+      resolve:{
+        selectedId: function(){
+          return $scope.selectedId;
+        }
+      }
+    });
+    
+    modalInstance.result.then(function() {
+      $scope.getUsers();
+    }, function() {
+    });
+  };
+  
   $scope.delUser = function() {
-    //alert($scope.selectedId);
+    alert($scope.selectedId);
     $http.delete('/users/' + $scope.selectedId)
       .success(function(data) {
         $scope.getUsers();
--- routes/users.js
+++ routes/users.js
@@ -3,62 +3,6 @@ var router = express.Router();
 var User = require('./data');
 
 var users = [];
-/*
-var users = [
-  {
-    id: 0,
-    firstname: 'Jack',
-    lastname: 'Sparrow',
-    gender: 'M',
-    cell: '1234567',
-    email: 'jack@gmail.com',
-    coords: {
-        latitude: 47.618819,
-        longitude: -122.168126
-      },
-      location: 'Bellevue'
-  },
-  {
-    id: 1,
-    firstname: 'Don',
-    lastname: 'Omar',
-    gender: 'M',
-    cell: '2346543',
-    email: 'don@hotmail.com',
-    coords:{
-        latitude: 47.673406,
-        longitude: -122.119031
-      },
-      location: 'Redmond'
-  },
-  {
-    id: 2,
-    firstname: 'Alice',
-    lastname: 'Cooper',
-    gender: 'F',
-    cell: '0322244',
-    email: 'alice@live.com',
-    coords:{
-        latitude: 47.616042, 
-        longitude: -122.042813
-      },
-      location: 'Sammamish'
-  },
-  {
-    id: 3,
-    firstname: 'Jerry',
-    lastname: 'Shawn',
-    gender: 'F',
-    cell: '0322244',
-    email: 'jerry@live.com',
-    coords:{
-        latitude: 47.569770,  
-        longitude: -122.223569
-      },
-      location: 'Mercer Island'
-  },
-];
-*/
 
 var sort_users = function(users) {
   var i = 0;
@@ -89,12 +33,9 @@ var sort_users = function(users) {
   return users;
 };
 
-
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //sort_users(users);
-  //res.json(users);
   User.find({}, function(err, users){
     if(err) throw err;
     
@@ -102,8 +43,9 @@ router.get('/', function(req, res, next) {
   });
 });
 
-//var currentId = users.length;
+
 var currentId = 0;
+
 router.post('/', function(req, res, next) {
   
   var newUser = new User({
@@ -128,30 +70,57 @@ router.post('/', function(req, res, next) {
   res.json({ result: 'done' });
 });
 
+//Modify Selected User
+router.post('/:id', function(req, res, next) {
+  console.log("Update User ID: " + req.params.id);
+  console.log("req.body.location: "+ req.body.location);
+  User.update({id: req.params.id},
+    {$set:{
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    gender: req.body.gender,
+    cell: req.body.cell,
+    email: req.body.email,
+    coords:req.body.coords,
+    location: req.body.location     
+    }},
+    {upsert: true},
+    function(err) {
+      if(err) throw err;
+      console.log("UserInfo updated Successfully!");
+    }
+    
+  );
+  /*
+   var UserInfo = new User({
+    id: req.params.id,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    gender: req.body.gender,
+    cell: req.body.cell,
+    email: req.body.email,
+    coords:req.body.coords,
+    location: req.body.location    
+  });
+  var upsertData = UserInfo.toObject();
+  delete upsertData._id;
+  User.update(req.params.id,upsertData,{upsert: true}, function(err){
+    if(err) throw err;
+    console.log("User Updated!");
+  });
+*/
+  res.json({ result: 'done' });
+});
+
 router.delete('/:id', function(req, res, next) {
   console.log(req.params.id);
-  //console.log(req.params.firstname);
   User.remove({id: req.params.id}, function(err,user){
     if(err) throw err;
-    console.log('User : ' + user + ' Removed!');
+    console.log('User Removed!');
   });
   res.json({ result: 'done' });
 });
 
-/*
-router.delete('/:id', function(req, res, next) {
-  //console.log(req.params.id);
-  var idx = -1;
-  for (var i = users.length - 1; i >= 0; i--) {
-    console.log(users[i].id, req.params.id);
-    if (users[i].id.toString() === req.params.id) {
-      idx = i;
-      break;
-    }
-  }
-  users.splice(idx, 1);
-  res.json({ result: 'done' });
-});
-*/
+
 
 module.exports = router;
